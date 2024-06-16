76b54e89b9c6beff1ca9bbb0b3a1e7e219badae6
ryanhancox <118982616+ryanhancox@users.noreply.github.com>
2024-06-16 16:55:07 +0100

    Update streamlit_app.py
    changed session object name to new connection object

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write("The name on your smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 connection = cnx.session()
-my_df = session.table("smoothies.public.fruit_options")\
+my_df = connection.table("smoothies.public.fruit_options")\
     .select(col("FRUIT_NAME"))
 #st.dataframe(data=my_df, use_container_width=True)
 
@@ -40,5 +40,5 @@ if ingredients_list:
     time_to_insert = st.button("Submit Order")
 
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        connection.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
